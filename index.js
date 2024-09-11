e1979896278612ef0f98e5b5ff9c21f271539a00
beatriz ayade <biaayade@hotmail.com>
2024-09-11 18:01:35 -0300

    inserir meta

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
+
+// Inicialize `metas` como um array vazio
+let metas = []
+
+const cadastrarMeta = async () => {
+    // Corrigido: Use o nome da variável `meta` na função sem conflito
+    const novaMeta = await input({ message: "insira a sua meta aqui: " })
+
+    if (novaMeta.length === 0) {  // Corrigido: Use `===` para comparação
+        console.log('este campo não pode estar vazio!')
+        return
+    }
+
+    metas.push(
+        { value: novaMeta, checked: false }
+    )
+}
 
 const start = async () => {
     while (true) {
@@ -6,12 +23,12 @@ const start = async () => {
             message: "menu",
             choices: [
                 {
-                    name: "vamos cadastrar",
-                    value: "cadastrar"
-                },
-                {
                     name: "inserir metas",
                     value: "inserir"
+                },
+                {
+                    name: "adicionar metas",
+                    value: "adicionar"
                 },
                 {
                     name: "sair",
@@ -21,11 +38,12 @@ const start = async () => {
         })
 
         switch (opcao) {
-            case "cadastrar":
-                console.log("vamos cadastrar")
-                break
             case "inserir":
-                console.log("inserir metas")
+                await cadastrarMeta()
+                console.log(metas)
+                break
+            case "adicionar":
+                console.log("adicionar metas")
                 break
             case "sair":
                 console.log("até a próxima!")
@@ -33,5 +51,6 @@ const start = async () => {
         }
     }
 }
+
 
 start()
